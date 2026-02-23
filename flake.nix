39b472329bce286964384bfede9ba464395fd604
Matthew Wilding <mbwilding@gmail.com>
2026-02-23 11:04:59 +0800

    update

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       inputs.home-manager.follows = "home-manager";
     };
 
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
