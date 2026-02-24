2a96e3c0d038dfdfcfe43b6e6554fe464aa071cd
Matthew Wilding <mbwilding@gmail.com>
2026-02-24 17:01:04 +0800

    update

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -38,6 +38,11 @@
       AWS_REGION = "ap-southeast-2";
       PULUMI_CONFIG_PASSPHRASE = "";
       DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+      DOTNET_ROOT = "${pkgs.dotnetCorePackages.combinePackages [
+        pkgs.dotnetCorePackages.dotnet_8.sdk
+        pkgs.dotnetCorePackages.dotnet_9.sdk
+        pkgs.dotnetCorePackages.dotnet_10.sdk
+      ]}/share/dotnet";
       NIXOS_OZONE_WL = "1";
     };
 
