735eeab5e933329d730b7a8b8c75bbe041d026f0
Matthew Wilding <mbwilding@gmail.com>
2026-02-24 23:52:51 +0800

    2026-02-24-23-52-51

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -9,11 +9,21 @@
     settings = {
       mgr = {
         linemode = "permissions";
+        scrolloff = 3;
         show_hidden = true;
+        show_symlink = true;
         sort_by = "alphabetical";
         sort_dir_first = true;
         sort_reverse = false;
         sort_sensitive = true;
+        title_format = "{cwd}";
+        mouse_events = [
+          "click"
+          "scroll"
+          "touch"
+          "move"
+          "drag"
+        ];
       };
     };
   };
