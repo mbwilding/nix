63244b24693f44269cbbd2ff9a8695125f28dafb
Matthew Wilding <mbwilding@gmail.com>
2026-02-24 23:50:03 +0800

    update

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -8,10 +8,12 @@
     shellWrapperName = "y";
     settings = {
       mgr = {
+        linemode = "permissions";
         show_hidden = true;
         sort_by = "alphabetical";
         sort_dir_first = true;
-        sort_reverse = true;
+        sort_reverse = false;
+        sort_sensitive = true;
       };
     };
   };
