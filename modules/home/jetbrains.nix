5a3af3d94b901ec10e2cad7065356980f7402233
Matthew Wilding <mbwilding@gmail.com>
2026-02-25 21:18:33 +0800

    2026-02-25-21-18-33

--- modules/home/jetbrains.nix
+++ modules/home/jetbrains.nix
@@ -1,8 +1,38 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
+let
+  dotnet = pkgs.dotnetCorePackages.combinePackages [
+    pkgs.dotnetCorePackages.dotnet_9.sdk
+  ];
+
+  rider =
+    (pkgs.jetbrains.rider.override {
+      forceWayland = true;
+    }).overrideAttrs
+      (old: {
+        postInstall =
+          (old.postInstall or "")
+          + lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
+            for dir in $out/rider/lib/ReSharperHost/linux-*; do
+              rm -rf $dir/dotnet
+              ln -s ${dotnet}/share/dotnet $dir/dotnet
+            done
+          '';
+        postFixup = (old.postFixup or "") + ''
+          wrapProgram $out/rider/bin/rider \
+            --prefix PATH : "${
+              lib.makeBinPath [
+                dotnet
+                pkgs.mono
+                pkgs.msbuild
+              ]
+            }"
+        '';
+      });
+in
 {
-  home.packages = with pkgs; [
-    jetbrains.datagrip
-    jetbrains.rider
+  home.packages = [
+    (pkgs.jetbrains.datagrip.override { forceWayland = true; })
+    rider
   ];
 }
