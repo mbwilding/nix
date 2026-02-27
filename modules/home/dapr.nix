919223acd2cd3e705fa33a447038090ed70e952e
Matthew Wilding <mbwilding@gmail.com>
2026-02-27 12:08:22 +0800

    update

--- modules/home/dapr.nix
+++ modules/home/dapr.nix
@@ -3,7 +3,7 @@
 {
   home.activation.daprInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if ${pkgs.dapr-cli}/bin/dapr version 2>/dev/null | grep -q "Runtime version: n/a"; then
-      ${pkgs.dapr-cli}/bin/dapr init --slim --container-runtime podman
+      ${pkgs.dapr-cli}/bin/dapr init --slim
     fi
   '';
 }
