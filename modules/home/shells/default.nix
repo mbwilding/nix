1dbe6edd7cf0d4c8183441d7a5ad647f5eefc852
Matthew Wilding <mbwilding@gmail.com>
2026-02-27 22:49:37 +0800

    2026-02-27-22-49-37

--- modules/home/shells/default.nix
+++ modules/home/shells/default.nix
@@ -8,19 +8,23 @@
     ./fish.nix
   ];
 
-  home.sessionVariables = {
-    GITLAB_TOKEN = secrets.gitlabWorkToken;
-    GITLAB_TOKEN_WORK = secrets.gitlabWorkToken;
-    GITHUB_TOKEN = secrets.githubWorkToken;
-    GITHUB_TOKEN_WORK = secrets.githubWorkToken;
-    GITHUB_TOKEN_PERSONAL = secrets.githubPersonalToken;
-    CARGO_REGISTRY_TOKEN = secrets.cargoToken;
-    ELEVENLABS_API_KEY = secrets.elevenLabsKey;
-    PULUMI_ACCESS_TOKEN = secrets.pulumiToken;
-    STEAM_API_KEY = secrets.steamToken;
-    WEATHER_API_TOKEN = secrets.weatherKey;
-    ANTHROPIC_API_KEY = secrets.anthropicKey;
-    DEEPSEEK_API_KEY = secrets.deepSeekKey;
-    OPENAI_API_KEY = secrets.openAiKey;
+  home = {
+    sessionPath = [ "$HOME/.cargo/bin" ];
+
+    sessionVariables = {
+      GITLAB_TOKEN = secrets.gitlabWorkToken;
+      GITLAB_TOKEN_WORK = secrets.gitlabWorkToken;
+      GITHUB_TOKEN = secrets.githubWorkToken;
+      GITHUB_TOKEN_WORK = secrets.githubWorkToken;
+      GITHUB_TOKEN_PERSONAL = secrets.githubPersonalToken;
+      CARGO_REGISTRY_TOKEN = secrets.cargoToken;
+      ELEVENLABS_API_KEY = secrets.elevenLabsKey;
+      PULUMI_ACCESS_TOKEN = secrets.pulumiToken;
+      STEAM_API_KEY = secrets.steamToken;
+      WEATHER_API_TOKEN = secrets.weatherKey;
+      ANTHROPIC_API_KEY = secrets.anthropicKey;
+      DEEPSEEK_API_KEY = secrets.deepSeekKey;
+      OPENAI_API_KEY = secrets.openAiKey;
+    };
   };
 }
