525dfb36ce0628bcfe1c21565f438882fd846cb6
Matthew Wilding <mbwilding@gmail.com>
2026-02-27 12:16:19 +0800

    update

--- modules/home/wine.nix
+++ modules/home/wine.nix
@@ -33,9 +33,9 @@ in
       winetricks
     ];
 
-    activation.winetricksCorefonts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    activation.winetricksInstalls = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       export PATH="${pkgs.wineWow64Packages.staging}/bin:$PATH"
-      ${pkgs.winetricks}/bin/winetricks --country=AU --optout --unattended ${lib.concatStringsSep " " installs}
+      ${pkgs.winetricks}/bin/winetricks --country=AU --optout --unattended ${lib.concatStringsSep " " installs} > /dev/null 2>&1
     '';
   };
 }
