0376930289cdd8da68994c03bc1421ac4a361367
Matthew Wilding <mbwilding@gmail.com>
2026-02-27 11:14:08 +0800

    update

--- modules/home/wine.nix
+++ modules/home/wine.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   home = {
@@ -6,5 +6,18 @@
       wineWow64Packages.staging
       winetricks
     ];
+
+    activation.winetricksCorefonts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if ! ${pkgs.wineWow64Packages.staging}/bin/wine --version >/dev/null 2>&1; then
+        echo "Wine not found: skipping winetricks corefonts"
+        exit 0
+      fi
+      if ! ${pkgs.wineWow64Packages.staging}/bin/wine "C:\\Windows\\Fonts\\arial.ttf" >/dev/null 2>&1; then
+        echo "Installing corefonts via winetricks..."
+        ${pkgs.winetricks}/bin/winetricks corefonts
+      else
+        echo "Corefonts already installed"
+      fi
+    '';
   };
 }
