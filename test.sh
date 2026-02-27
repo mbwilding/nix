30651b56166e87b3a392c5b472ec319fd0d34d72
Matthew Wilding <mbwilding@gmail.com>
2026-02-27 10:04:50 +0800

    update

--- test.sh
+++
@@ -1,8 +0,0 @@
-# aws ecr get-login-password --profile cd-read | docker login --username AWS --password-stdin https://849210807105.dkr.ecr.ap-southeast-2.amazonaws.com
-base="849210807105.dkr.ecr.ap-southeast-2.amazonaws.com/rwwa.awesomecapability/api"
-src="${base}:latest"
-dst="${base}:devl-0.0.1"
-# docker buildx imagetools create --append --tag $dst $src
-sudo docker manifest inspect "${src}"
-# sudo docker tag "${src}" "${dst}"
-# sudo docker push "${dst}"
