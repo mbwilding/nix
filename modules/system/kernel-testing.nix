9e33274b17c74b355cc510c8e0353f5f39fabe4c
Matthew Wilding <mbwilding@gmail.com>
2026-03-11 23:33:43 +0800

    kernel 7

--- modules/system/kernel-testing.nix
+++ modules/system/kernel-testing.nix
@@ -5,12 +5,14 @@
     (final: prev: {
       linuxKernel = prev.linuxKernel // {
         kernels = prev.linuxKernel.kernels // {
-          linux_testing = prev.linuxKernel.kernels.linux_testing.overrideAttrs {
-            version = "7.0-rc3";
-            modDirVersion = "7.0.0-rc3";
-            src = prev.fetchzip {
-              url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-7.0-rc3.tar.gz";
-              hash = "sha256:1f9rkk1h1m84yglxgicasmdgywim7zc2ndn0ya7wm27kc8f3whw5";
+          linux_testing = prev.linuxKernel.kernels.linux_testing.override {
+            argsOverride = {
+              version = "7.0-rc3";
+              modDirVersion = "7.0.0-rc3";
+              src = prev.fetchzip {
+                url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-7.0-rc3.tar.gz";
+                hash = "sha256:1f9rkk1h1m84yglxgicasmdgywim7zc2ndn0ya7wm27kc8f3whw5";
+              };
             };
           };
         };
