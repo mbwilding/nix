1c3ee2f54c7a4ea45a494676a20612af06bf8d62
Matthew Wilding <mbwilding@gmail.com>
2026-03-15 14:37:32 +0800

    2026-03-15-14-37-32

--- modules/system/secrets.nix
+++ modules/system/secrets.nix
@@ -1,58 +1,60 @@
+{
+  home ? builtins.getEnv "HOME",
+}:
+
 let
   readSecret =
     path:
     if builtins.pathExists path then builtins.readFile path else throw "Secret file not found: ${path}";
 
-  readSecretJSON =
-    path:
-    builtins.fromJSON (readSecret path);
+  readSecretJSON = path: builtins.fromJSON (readSecret path);
 in
 {
   # SSH keys
-  personalPublicKey = readSecret /home/anon/.ssh/personal.pub;
-  workPublicKey = readSecret /home/anon/.ssh/work.pub;
+  personalPublicKey = readSecret "${home}/.ssh/personal.pub";
+  workPublicKey = readSecret "${home}/.ssh/work.pub";
 
   # Work identity
-  workName = readSecret /home/anon/.secrets/work-name;
-  workId = readSecret /home/anon/.secrets/work-id;
-  workEmailName = readSecret /home/anon/.secrets/work-email-name;
-  workEmailId = readSecret /home/anon/.secrets/work-email-id;
+  workName = readSecret "${home}/.secrets/work-name";
+  workId = readSecret "${home}/.secrets/work-id";
+  workEmailName = readSecret "${home}/.secrets/work-email-name";
+  workEmailId = readSecret "${home}/.secrets/work-email-id";
 
   # GitHub
-  githubWorkUsername = readSecret /home/anon/.secrets/github-work-username;
-  githubWorkToken = readSecret /home/anon/.secrets/github-work;
-  githubPersonalToken = readSecret /home/anon/.secrets/github-personal;
+  githubWorkUsername = readSecret "${home}/.secrets/github-work-username";
+  githubWorkToken = readSecret "${home}/.secrets/github-work";
+  githubPersonalToken = readSecret "${home}/.secrets/github-personal";
 
   # GitLab
-  gitlabWorkToken = readSecret /home/anon/.secrets/gitlab-work;
+  gitlabWorkToken = readSecret "${home}/.secrets/gitlab-work";
 
   # Cargo
-  cargoToken = readSecret /home/anon/.secrets/cargo;
+  cargoToken = readSecret "${home}/.secrets/cargo";
 
   # API keys
-  anthropicKey = readSecret /home/anon/.secrets/anthropic;
-  deepSeekKey = readSecret /home/anon/.secrets/deepseek;
-  openAiKey = readSecret /home/anon/.secrets/openai;
-  elevenLabsKey = readSecret /home/anon/.secrets/elevenlabs;
-  weatherKey = readSecret /home/anon/.secrets/weather;
+  anthropicKey = readSecret "${home}/.secrets/anthropic";
+  deepSeekKey = readSecret "${home}/.secrets/deepseek";
+  openAiKey = readSecret "${home}/.secrets/openai";
+  elevenLabsKey = readSecret "${home}/.secrets/elevenlabs";
+  weatherKey = readSecret "${home}/.secrets/weather";
 
   # Networks
-  wifiHomeSsid = readSecret /home/anon/.secrets/wifi-home-ssid;
-  wifiHomePassword = readSecret /home/anon/.secrets/wifi-home-password;
-  wifiParentsSsid = readSecret /home/anon/.secrets/wifi-parents-ssid;
-  wifiParentsPassword = readSecret /home/anon/.secrets/wifi-parents-password;
+  wifiHomeSsid = readSecret "${home}/.secrets/wifi-home-ssid";
+  wifiHomePassword = readSecret "${home}/.secrets/wifi-home-password";
+  wifiParentsSsid = readSecret "${home}/.secrets/wifi-parents-ssid";
+  wifiParentsPassword = readSecret "${home}/.secrets/wifi-parents-password";
 
   # Vault
-  password = readSecret /home/anon/.secrets/password;
+  password = readSecret "${home}/.secrets/password";
 
   # Infrastructure
-  pulumiToken = readSecret /home/anon/.secrets/pulumi;
-  steamToken = readSecret /home/anon/.secrets/steam;
-  homeIp = readSecret /home/anon/.secrets/home-ip;
-  wireguardPrivateKey = readSecret /home/anon/.secrets/home-wireguard-private-key;
-  wireguardPublicKey = readSecret /home/anon/.secrets/home-wireguard-public-key;
-  wireguardEndpoint = readSecret /home/anon/.secrets/home-wireguard-endpoint;
+  pulumiToken = readSecret "${home}/.secrets/pulumi";
+  steamToken = readSecret "${home}/.secrets/steam";
+  homeIp = readSecret "${home}/.secrets/home-ip";
+  wireguardPrivateKey = readSecret "${home}/.secrets/home-wireguard-private-key";
+  wireguardPublicKey = readSecret "${home}/.secrets/home-wireguard-public-key";
+  wireguardEndpoint = readSecret "${home}/.secrets/home-wireguard-endpoint";
 
   # AWS
-  aws = readSecretJSON /home/anon/.secrets/aws.json;
+  aws = readSecretJSON "${home}/.secrets/aws.json";
 }
