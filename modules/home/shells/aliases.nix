731839af27abca439bec24f71a7777bdb8bce0ff
Matthew Wilding <mbwilding@gmail.com>
2026-03-19 01:01:18 +0800

    2026-03-19-01-01-18

--- modules/home/shells/aliases.nix
+++ modules/home/shells/aliases.nix
@@ -2,31 +2,32 @@
 
 {
   home.shellAliases = {
+    ${secrets.workName} = "proxychains4 -q -f ~/.config/proxychains/proxychains.conf";
     awsl = "aws sso login --sso-session ${secrets.workName}";
     azl = "az login --scope https://graph.microsoft.com/.default --allow-no-subscriptions";
+    battery = "cat /sys/class/power_supply/BAT1/capacity";
     bios = "systemctl reboot --firmware-setup";
     c = "clear";
+    g = "git";
     grep = "grep --color";
+    hm-expire = "home-manager expire-generations -days";
     lg = "lazygit";
     ll = "eza -lhg";
     lla = "eza -alhg";
     ls = "eza";
     n = "nvim";
-    nmr = "nmcli radio wifi off && nmcli radio wifi on";
-    oc = "opencode";
-    q = "exit";
-    hm-expire = "home-manager expire-generations -days";
     nix-build = "sudo nixos-rebuild build --impure --flake ~/nix";
     nix-clean = "sudo nix-collect-garbage -d";
-    power-p = "powerprofilesctl set performance";
+    nmr = "nmcli radio wifi off && nmcli radio wifi on";
+    oc = "opencode";
     power-b = "powerprofilesctl set balanced";
+    power-p = "powerprofilesctl set performance";
     power-s = "powerprofilesctl set power-saver";
-    battery = "cat /sys/class/power_supply/BAT1/capacity";
-    wifi-list = "nmcli device wifi list";
-    ${secrets.workName} = "proxychains4 -q -f ~/.config/proxychains/proxychains.conf";
+    q = "exit";
     t = "zellij";
     tree = "eza --tree";
     wgd = "sudo systemctl stop wg-quick-Home";
     wgu = "sudo systemctl start wg-quick-Home";
+    wifi-list = "nmcli device wifi list";
   };
 }
