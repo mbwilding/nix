6f3b47d803614bda3c0c2d0ab603784b027ef26d
Matthew Wilding <mbwilding@gmail.com>
2026-03-25 08:06:58 +0800

    update

--- modules/system/waydroid.nix
+++ modules/system/waydroid.nix
@@ -2,12 +2,19 @@
 
 {
   virtualisation.waydroid.enable = true;
+  networking.nftables.enable = true;
   services.geoclue2.enable = true;
   programs.kdeconnect.enable = true;
 
   environment = {
     systemPackages = with pkgs; [
       android-tools
+      waydroid-helper
     ];
+  };
+
+  systemd = {
+    packages = [ pkgs.waydroid-helper ];
+    services.waydroid-mount.wantedBy = [ "multi-user.target" ];
   };
 }
