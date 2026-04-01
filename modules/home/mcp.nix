9615f623be1790c709ebe839fb5c9fc5321da91c
Matthew Wilding <mbwilding@gmail.com>
2026-04-01 15:59:06 +0800

    update

--- modules/home/mcp.nix
+++ modules/home/mcp.nix
@@ -3,39 +3,42 @@
 let
   base64 = import ../helpers/base64.nix { inherit lib; };
   atlassian = base64.toBase64 "${secrets.workEmailId}:${secrets.atlassianKey}";
-in
-{
-  programs = {
-    mcp = {
-      enable = true;
-      servers = {
-        github-work = {
-          type = "http";
-          url = "https://api.githubcopilot.com/mcp";
-          headers = {
-            Authorization = "Bearer ${secrets.githubWorkToken}";
-          };
-        };
-        github-personal = {
-          type = "http";
-          url = "https://api.githubcopilot.com/mcp";
-          headers = {
-            Authorization = "Bearer ${secrets.githubPersonalToken}";
-          };
-        };
-        atlassian = {
-          type = "http";
-          url = "https://mcp.atlassian.com/v1/mcp";
-        };
-        lucid = {
-          type = "http";
-          url = "https://mcp.lucid.app/mcp";
-        };
-        # figma = {
-        #   type = "http";
-        #   url = "https://mcp.figma.com/mcp";
-        # };
+
+  mcpServers = {
+    github-work = {
+      type = "http";
+      url = "https://api.githubcopilot.com/mcp";
+      headers = {
+        Authorization = "Bearer ${secrets.githubWorkToken}";
       };
     };
+    github-personal = {
+      type = "http";
+      url = "https://api.githubcopilot.com/mcp";
+      headers = {
+        Authorization = "Bearer ${secrets.githubPersonalToken}";
+      };
+    };
+    atlassian = {
+      type = "http";
+      url = "https://mcp.atlassian.com/v1/mcp";
+    };
+    lucid = {
+      type = "http";
+      url = "https://mcp.lucid.app/mcp";
+    };
+    # figma = {
+    #   type = "http";
+    #   url = "https://mcp.figma.com/mcp";
+    # };
   };
+in
+{
+  programs.mcp = {
+    enable = true;
+    servers = mcpServers;
+  };
+
+  home.file.".config/.copilot/mcp-config.json".text =
+    builtins.toJSON { mcpServers = mcpServers; };
 }
