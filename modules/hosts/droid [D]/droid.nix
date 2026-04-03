1136a82d5e99fdfc04f3a9034e2496d9a55bd2e4
Matthew Wilding <mbwilding@gmail.com>
2026-04-04 02:10:13 +0800

    2026-04-04-02-10-13

--- modules/hosts/droid [D]/droid.nix
+++ modules/hosts/droid [D]/droid.nix
@@ -14,7 +14,7 @@
       environment.etcBackupExtension = ".bak";
 
       # Pin nix to 2.31.2 — versions >=2.31.3 break PTY on Android (nix-on-droid#495)
-      nix.package = lib.mkForce pkgs.nixVersions.nix_2_30;
+      nix.package = lib.mkForce pkgs.nixVersions.nix_2_20;
 
       nix.extraOptions = ''
         experimental-features = nix-command flakes
