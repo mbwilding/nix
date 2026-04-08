c8c3c3a664ac4bb2442081ade5b931d6532f84fd
Matthew Wilding <mbwilding@gmail.com>
2026-04-08 13:12:01 +0800

    Jira

--- modules/programs/opencode [n]/opencode.nix
+++ modules/programs/opencode [n]/opencode.nix
@@ -25,8 +25,6 @@
             story-writer = ''
               # Story Writer Agent
 
-              Plan mode is used until the user has confirmed the plan, then we action.
-
               ## Local MCP Servers:
 
               - github-work
@@ -42,10 +40,14 @@
 
               ## How to Use This Agent
 
+              Plan mode is used until the user has confirmed the plan, then we action.
+
               Use the agent `feature-design.agent.md` as your additional system prompt.
 
               Use the `create-feature-design/Skill.md` skill to carry out the full creation workflow. The skill
               contains detailed step-by-step instructions.
+
+              Ask questions using your built in question logic to ask for more info or if you don't understand something.
 
               You may also be invoked directly with a prompt like:
               - "Create a feature design for [feature description]"
