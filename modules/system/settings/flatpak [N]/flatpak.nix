a127cd18bcbe4e877b54df50307cfabc8baf6045
Matthew Wilding <mbwilding@gmail.com>
2026-04-28 17:14:27 +0800

    Update

--- modules/system/settings/flatpak [N]/flatpak.nix
+++ modules/system/settings/flatpak [N]/flatpak.nix
@@ -5,12 +5,13 @@
     home-manager.sharedModules = [
       inputs.nix-flatpak.homeManagerModules.nix-flatpak
       {
-        services.flatpak.packages = [
-          {
-            appId = "com.hytale.Hytale";
-            origin = "flathub";
-          }
-        ];
+        # NOTE: Example
+        # services.flatpak.packages = [
+        #   {
+        #     appId = "com.hytale.Hytale";
+        #     origin = "flathub";
+        #   }
+        # ];
       }
     ];
   };
