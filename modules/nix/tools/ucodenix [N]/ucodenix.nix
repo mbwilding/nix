da3c5daede063185b4bf48e27f52abb90a651254
Matthew Wilding <mbwilding@gmail.com>
2026-05-01 10:45:01 +0800

    ucodenix

--- modules/nix/tools/ucodenix [N]/ucodenix.nix
+++ modules/nix/tools/ucodenix [N]/ucodenix.nix
@@ -4,11 +4,33 @@
 }:
 
 {
-  flake.modules.nixos.ucodenix = {
-    imports = [
-      inputs.ucodenix.nixosModules.default
-    ];
+  flake.modules.nixos.ucodenix =
+    { pkgs, lib, ... }:
+    let
+      isAmd =
+        let
+          vendorLine = lib.findFirst (lib.hasPrefix "vendor_id") "" (
+            lib.splitString "\n" (builtins.readFile "/proc/cpuinfo")
+          );
+        in
+        lib.hasSuffix "AuthenticAMD" vendorLine;
 
-    services.ucodenix.enable = true;
-  };
+      cpuModelId = lib.removeSuffix "\n" (
+        builtins.readFile (
+          pkgs.runCommand "cpuid-model" { buildInputs = [ pkgs.cpuid ]; } ''
+            cpuid -1 -l 1 -r | sed -n 's/.*eax=0x\([0-9a-f]*\).*/\U\1/p' > $out
+          ''
+        )
+      );
+    in
+    {
+      imports = [
+        inputs.ucodenix.nixosModules.default
+      ];
+
+      services.ucodenix = lib.mkIf isAmd {
+        enable = true;
+        inherit cpuModelId;
+      };
+    };
 }
