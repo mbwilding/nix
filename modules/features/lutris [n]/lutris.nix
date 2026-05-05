1fea8f2456062d055f924712e851ce091cd6abf0
Matthew Wilding <mbwilding@gmail.com>
2026-05-05 12:59:18 +0800

    Update

--- modules/features/lutris [n]/lutris.nix
+++ modules/features/lutris [n]/lutris.nix
@@ -20,8 +20,13 @@
   flake.modules.homeManager.lutris =
     { pkgs, ... }:
     {
-      home.packages = with pkgs; [
-        lutris
-      ];
+      programs = {
+        lutris = {
+          enable = true;
+          protonPackages = [
+            pkgs.proton-ge-bin
+          ];
+        };
+      };
     };
 }
