aa181ef039aa1b8501ece0e7c9551bd67382b9b9
Matthew Wilding <mbwilding@gmail.com>
2026-05-13 10:43:29 +0800

    Dotnet/Rider?

--- modules/features/jetbrains [n]/jetbrains.nix
+++ modules/features/jetbrains [n]/jetbrains.nix
@@ -2,46 +2,12 @@
 
 {
   flake.modules.homeManager.jetbrains =
-    {
-      lib,
-      pkgs,
-      pkgsMaster,
-      config,
-      ...
-    }:
+    { pkgsMaster, ... }:
 
-    let
-      dotnet = config.custom.dotnet.sdk;
-
-      rider =
-        (pkgsMaster.jetbrains.rider.override {
-          forceWayland = true;
-        }).overrideAttrs
-          (old: {
-            postInstall =
-              (old.postInstall or "")
-              + lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-                for dir in $out/rider/lib/ReSharperHost/linux-*; do
-                  rm -rf $dir/dotnet
-                  ln -s ${dotnet}/share/dotnet $dir/dotnet
-                done
-              '';
-            postFixup = (old.postFixup or "") + ''
-              wrapProgram $out/rider/bin/rider \
-                --prefix PATH : "${
-                  lib.makeBinPath [
-                    dotnet
-                    pkgs.mono
-                    pkgs.msbuild
-                  ]
-                }"
-            '';
-          });
-    in
     {
       home.packages = [
         (pkgsMaster.jetbrains.datagrip.override { forceWayland = true; })
-        rider
+        (pkgsMaster.jetbrains.rider.override { forceWayland = true; })
       ];
     };
 }
