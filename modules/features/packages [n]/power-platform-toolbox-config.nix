9c6698d4521eee3828734c0e002470bcf9f8e981
Matthew Wilding <mbwilding@gmail.com>
2026-05-25 12:01:50 +0800

    pptb

--- modules/features/packages [n]/power-platform-toolbox-config.nix
+++ modules/features/packages [n]/power-platform-toolbox-config.nix
@@ -2,33 +2,50 @@
 
 {
   flake.modules.homeManager.power-platform-toolbox =
-    { lib, pkgs, secrets, ... }:
+    {
+      lib,
+      pkgs,
+      secrets,
+      ...
+    }:
     let
       connectionsFile = pkgs.writeText "connections.json" (builtins.toJSON secrets.dynamicsCredentials);
-      userSettingsFile = pkgs.writeText "user-settings.json" (builtins.toJSON {
-        theme = "system";
-        language = "en";
-        autoUpdate = false;
-        terminalFont = "NeoSpleen Nerd Font";
-        notificationDuration = 5000;
-        showDebugMenu = true;
-        deprecatedToolsVisibility = "hide-all";
-        connections = [ ];
-        installedTools = [
-          "179a21f4-c89c-4757-8c57-0fd8c7d47ddb"
-          "d4b881a6-d7ac-401e-a6f8-5830af903da8"
-          "de178113-27a1-4acb-b8f4-6b62ef0021e0"
-          "0db0368c-e0dd-4575-bdf0-7495a43ef660"
-        ];
-        lastUsedTools = [ ];
-        favoriteTools = [ ];
-        cspConsents = { };
-        toolConnections = { };
-        toolSecondaryConnections = { };
-        connectionsSort = "last-used";
-        restoreSessionOnStartup = true;
-        installId = "37e6a270-227d-48e6-ab31-2736ae4982b6";
-      });
+      userSettingsFile = pkgs.writeText "user-settings.json" (
+        builtins.toJSON {
+          autoUpdate = false;
+          connections = [ ];
+          connectionsSort = "last-used";
+          cspConsents = {
+            de178113-27a1-4acb-b8f4-6b62ef0021e0 = {
+              allowed = true;
+              required = [
+                "https://raw.githubusercontent.com"
+                "https://raw.githubusercontent.com/LinkeD365/*"
+              ];
+              optional = [ ];
+            };
+          };
+          deprecatedToolsVisibility = "hide-all";
+          favoriteTools = [
+            "3219642b-638a-45c8-97d5-34d1fbc0d3ba"
+            "0db0368c-e0dd-4575-bdf0-7495a43ef660"
+          ];
+          installId = "37e6a270-227d-48e6-ab31-2736ae4982b6";
+          installedTools = [
+            "3219642b-638a-45c8-97d5-34d1fbc0d3ba"
+            "0db0368c-e0dd-4575-bdf0-7495a43ef660"
+          ];
+          language = "en";
+          lastUsedTools = [ ];
+          notificationDuration = 5000;
+          restoreSessionOnStartup = true;
+          showDebugMenu = true;
+          terminalFont = "NeoSpleen Nerd Font";
+          theme = "system";
+          toolConnections = { };
+          toolSecondaryConnections = { };
+        }
+      );
       configDir = "$HOME/.config/powerplatform-toolbox";
     in
     {
