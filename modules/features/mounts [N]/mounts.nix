fdc622fbd1cb69fce714c04e5b67edc8f6392dcb
Matthew Wilding <mbwilding@gmail.com>
2026-05-26 21:02:48 +0800

    2026-05-26-21-02-48

--- modules/features/mounts [N]/mounts.nix
+++ modules/features/mounts [N]/mounts.nix
@@ -5,15 +5,16 @@
     { ... }:
 
     {
-      fileSystems."/mnt/nfs/common" = {
-        device = "192.168.11.10:/mnt/main/Common";
-        fsType = "nfs";
-        options = [
-          "nofail"
-          "_netdev"
-          "x-systemd.automount"
-          "x-systemd.device-timeout=10"
-        ];
-      };
+      # NOTE: Causes boot failure
+      # fileSystems."/mnt/nfs/common" = {
+      #   device = "192.168.11.10:/mnt/main/Common";
+      #   fsType = "nfs";
+      #   options = [
+      #     "nofail"
+      #     "_netdev"
+      #     "x-systemd.automount"
+      #     "x-systemd.device-timeout=10"
+      #   ];
+      # };
     };
 }
