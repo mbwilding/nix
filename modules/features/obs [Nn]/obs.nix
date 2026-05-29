791b502ed121f00888ccba2ac7caa01f5286e149
Matthew Wilding <mbwilding@gmail.com>
2026-05-29 12:54:33 +0800

    OBS Virtual Mic

--- modules/features/obs [Nn]/obs.nix
+++ modules/features/obs [Nn]/obs.nix
@@ -22,11 +22,56 @@
         obs-studio
         v4l-utils
       ];
+
+      services.pipewire.extraConfig.pipewire."90-obs-virtual-audio" = {
+        "context.objects" = [
+          {
+            factory = "adapter";
+            args = {
+              "factory.name" = "support.null-audio-sink";
+              "node.name" = "obs-monitor-sink";
+              "node.description" = "OBS Monitor";
+              "media.class" = "Audio/Sink";
+              "audio.position" = "FL,FR";
+              "object.linger" = true;
+            };
+          }
+          {
+            factory = "adapter";
+            args = {
+              "factory.name" = "support.null-audio-sink";
+              "node.name" = "obs-virtual-mic";
+              "node.description" = "OBS Mic";
+              "media.class" = "Audio/Source/Virtual";
+              "audio.position" = "FL,FR";
+              "object.linger" = true;
+            };
+          }
+        ];
+      };
     };
 
   flake.modules.homeManager.obs =
     { pkgs, config, ... }:
     {
+      systemd.user.services.obs-virtual-mic-link = {
+        Unit = {
+          Description = "Link OBS monitor sink to OBS virtual mic";
+          After = [ "pipewire.service" ];
+          Requires = [ "pipewire.service" ];
+        };
+        Service = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+          ExecStart = pkgs.writeShellScript "obs-virtual-mic-link" ''
+            ${pkgs.pipewire}/bin/pw-link obs-monitor-sink:monitor_FL obs-virtual-mic:input_FL
+            ${pkgs.pipewire}/bin/pw-link obs-monitor-sink:monitor_FR obs-virtual-mic:input_FR
+          '';
+        };
+        Install.WantedBy = [ "default.target" ];
+      };
+
       programs.obs-studio = {
         enable = true;
         # plugins = [];
