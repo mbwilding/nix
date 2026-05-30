53417442c2d84fb2a77d4bf9cb4246c3f3198757
Matthew Wilding <mbwilding@gmail.com>
2026-05-30 16:23:13 +0800

    update

--- modules/features/packages [n]/_open-ecc.nix
+++ modules/features/packages [n]/_open-ecc.nix
@@ -1,38 +1,60 @@
 {
   lib,
-  rustPlatform,
-  fetchFromGitHub,
-  pkg-config,
-  openssl,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "open-ecc";
-  version = "0.0.6";
+let
+  version = "0.0.7";
 
-  src = fetchFromGitHub {
-    owner = "mbwilding";
-    repo = "open-ecc";
-    tag = "v${version}";
-    hash = "sha256-YkSIJgGczbBnRd+tdPOS+P9MtUOe2BPhoU4FOvOQwEA=";
+  sources = {
+    "x86_64-linux" = {
+      url = "https://github.com/mbwilding/open-ecc/releases/download/v${version}/ecc-Linux-musl-x86_64.tar.gz";
+      hash = "sha256-NaSVQ8WcbN2PEpNuaz/JEex/1T5mHgz7vQWsAdPR2gc=";
+    };
+    "aarch64-linux" = {
+      url = "https://github.com/mbwilding/open-ecc/releases/download/v${version}/ecc-Linux-musl-arm64.tar.gz";
+      hash = "sha256-cRrBWgY25owmtyCaNc8wXyhfshYhsbc8vgInuw/klJE=";
+    };
+    "aarch64-darwin" = {
+      url = "https://github.com/mbwilding/open-ecc/releases/download/v${version}/ecc-macOS-arm64.tar.gz";
+      hash = "sha256-sepO2Ws6qfQqEqGootKoqLF9TjB2sfDhgBaiQ6Rpcm4=";
+    };
+    "x86_64-darwin" = {
+      url = "https://github.com/mbwilding/open-ecc/releases/download/v${version}/ecc-macOS-x86_64.tar.gz";
+      hash = "sha256-I33KRaVTUKaYJG5zqjFMArLW5gqcVTOZU/QaOxJtMeA=";
+    };
   };
 
-  cargoHash = "sha256-W/V1xiUFEyEXhCpDoQjUxO2MKOiL+dTKJHrzfKvZ/PM=";
+  source =
+    sources.${stdenv.hostPlatform.system}
+      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+in
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ openssl ];
+stdenv.mkDerivation {
+  pname = "open-ecc";
+  inherit version;
 
-  env.OPENSSL_NO_VENDOR = 1;
+  src = fetchurl {
+    inherit (source) url hash;
+  };
 
-  cargoBuildFlags = [ "--package" "open_ecc_cli" ];
-  cargoTestFlags = [ "--package" "open_ecc_cli" ];
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    install -Dm755 ecc $out/bin/ecc
+  '';
 
   meta = with lib; {
     description = "Unofficial Elgato Command Centre cross-platform CLI";
     homepage = "https://github.com/mbwilding/open-ecc";
     license = licenses.mit;
-    platforms = platforms.all;
+    platforms = builtins.attrNames sources;
     maintainers = with maintainers; [ mbwilding ];
     mainProgram = "ecc";
+    sourceProvenance = [ sourceTypes.binaryNativeCode ];
   };
 }
