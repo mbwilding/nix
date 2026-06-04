5e017b6ef11e518ad94b96ed16b25bcb98e1c848
Matthew Wilding <mbwilding@gmail.com>
2026-06-04 09:51:07 +0800

    vesktop

--- modules/features/discord [n]/discord.nix
+++ modules/features/discord [n]/discord.nix
@@ -7,7 +7,35 @@
     {
       programs.vesktop = {
         enable = true;
-        vencord.settings.plugins.GameActivityToggle.enabled = true;
+        vencord = {
+          settings = {
+            plugins = {
+              AlwaysTrust.enabled = true;
+              BetterFolders.enabled = true;
+              BetterRoleContext.enabled = true;
+              BetterSessions.enabled = true;
+              BetterSettings.enabled = true;
+              BetterUploadButton.enabled = true;
+              BiggerStreamPreview.enabled = true;
+              CallTimer.enabled = true;
+              CharacterCount.enabled = true;
+              ClearURLs.enabled = true;
+              ConsoleJanitor.enabled = true;
+              CopyFileContents.enabled = true;
+              CustomIdle.enabled = true;
+              FixYoutubeEmbeds.enabled = true;
+              FriendsSince.enabled = true;
+              FullSearchContext.enabled = true;
+              GameActivityToggle.enabled = true;
+              ImageFilename.enabled = true;
+              ImageZoom.enabled = true;
+              MemberCount.enabled = true;
+              MentionAvatars.enabled = true;
+              MessageLatency.enabled = true;
+              VolumeBooster.enabled = true;
+            };
+          };
+        };
       };
     };
 }
