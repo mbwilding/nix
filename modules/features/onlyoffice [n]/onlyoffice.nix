ef1ca524bda4bd4137b4c072acafd1ee6df8e06a
Matthew Wilding <mbwilding@gmail.com>
2026-06-17 21:38:59 +0800

    update

--- modules/features/onlyoffice [n]/onlyoffice.nix
+++ modules/features/onlyoffice [n]/onlyoffice.nix
@@ -9,7 +9,9 @@
           enable = true;
           settings = {
             UITheme = "theme-night";
-            editorWindowMode = false;
+            appdata = "@ByteArray(eyJ1c2VybmFtZSI6Im1id2lsZGluZyIsImRvY29wZW5tb2RlIjoiZWRpdCIsInJlc3RhcnQiOmZhbHNlLCJsYW5naWQiOiJlbi1HQiIsInVpc2NhbGluZyI6IjE1MCIsInVpdGhlbWUiOiJ0aGVtZS1uaWdodCIsImVkaXRvcndpbmRvd21vZGUiOnRydWUsInNwZWxsY2hlY2tkZXRlY3QiOiJhdXRvIiwidXNlZ3B1Ijp0cnVlfQ==)";
+            editorWindowMode = true;
+            locale = "en-GB";
             titlebar = "custom";
           };
         };
