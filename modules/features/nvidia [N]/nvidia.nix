d64f7a2827e3406cd22031d85d12cae62e7fa003
Matthew Wilding <mbwilding@gmail.com>
2026-06-18 13:42:57 +0800

    Update

--- modules/features/nvidia [N]/nvidia.nix
+++ modules/features/nvidia [N]/nvidia.nix
@@ -15,15 +15,15 @@
         };
 
         nvidia = {
-          # package = config.boot.kernelPackages.nvidiaPackages.beta; # stable
-          package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-            version = "610.43.02";
-            sha256_64bit = "sha256:0qvllxnb20arjhw3bxdz0hw521di9ib75hldzx97gpscpdaa0d1h";
-            sha256_aarch64 = "sha256:0qvllxnb20arjhw3bxdz0hw521di9ib75hldzx97gpscpdaa0d1h";
-            openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
-            settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
-            persistencedSha256 = "sha256:0nd0bf2s9b2ic8a0rcscddasddkryx2qf6mx4861bv44wblm513z";
-          };
+          package = config.boot.kernelPackages.nvidiaPackages.stable; # beta
+          # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          #   version = "610.43.02";
+          #   sha256_64bit = "sha256:0qvllxnb20arjhw3bxdz0hw521di9ib75hldzx97gpscpdaa0d1h";
+          #   sha256_aarch64 = "sha256:0qvllxnb20arjhw3bxdz0hw521di9ib75hldzx97gpscpdaa0d1h";
+          #   openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
+          #   settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
+          #   persistencedSha256 = "sha256:0nd0bf2s9b2ic8a0rcscddasddkryx2qf6mx4861bv44wblm513z";
+          # };
           open = true;
           modesetting.enable = true;
           nvidiaSettings = true;
