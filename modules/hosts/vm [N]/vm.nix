e4c3deb6af6d2ad2bb6c9bfb0472ac9e22e9be58
Matthew Wilding <mbwilding@gmail.com>
2026-06-19 14:36:10 +0800

    2026-06-19-14-36-10

--- modules/hosts/vm [N]/vm.nix
+++ modules/hosts/vm [N]/vm.nix
@@ -1,25 +1,30 @@
 { inputs, ... }:
 
+let
+  arch = "x86_64-linux";
+  hostName = "vm";
+in
 {
   flake.modules.nixos.vm =
     { ... }:
     {
-      imports = with inputs.self.modules.nixos; [
-        system-default
-        user-mbwilding
+      imports =
+        with inputs.self.modules.nixos;
+        [
+          kde
+          system-default
+          user-mbwilding
+        ]
+        ++ [ ./_hardware-configuration.nix ];
 
-        # hyprland
-        kde
-      ] ++ [ ./_hardware-configuration.nix ];
-
-      networking.hostName = "vm";
+      networking.hostName = hostName;
 
       system.stateVersion = "25.11";
     };
 
-  flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "vm";
+  flake.nixosConfigurations = inputs.self.lib.mkNixos arch hostName;
 
-  flake.homeConfigurations = inputs.self.lib.mkHomeManager "x86_64-linux" "vm" [
+  flake.homeConfigurations = inputs.self.lib.mkHomeManager arch hostName [
     inputs.self.modules.homeManager.kde
   ];
 }
