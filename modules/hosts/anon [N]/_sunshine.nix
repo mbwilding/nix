6a9c3cb3808d6e6ec5815931cdb4889a30cf243d
Matthew Wilding <mbwilding@gmail.com>
2026-06-23 12:21:10 +0800

    Update

--- modules/hosts/anon [N]/_sunshine.nix
+++ modules/hosts/anon [N]/_sunshine.nix
@@ -11,7 +11,7 @@
       audio_sink = "alsa_output.usb-RME_Babyface_Pro__71972575__77EB3EDA0B95BC8-00.analog-stereo";
       install_steam_audio_drivers = "enabled";
       adapter_name = "/dev/dri/renderD128";
-      capture = "nvfbc";
+      capture = "kms";
       encoder = "nvenc";
       nvenc_preset = 1;
     };
