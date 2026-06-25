7eae54d33e437a9a187721c6bb4b847b422ba716
Matthew Wilding <mbwilding@gmail.com>
2026-06-25 21:43:46 +0800

    update

--- modules/features/applications/emacs [n]/emacs.nix
+++ modules/features/applications/emacs [n]/emacs.nix
@@ -19,10 +19,26 @@
           (menu-bar-mode 0)
           (scroll-bar-mode 0)
           (global-display-line-numbers-mode)
+          (display-line-numbers-type 'relative)
 
           (modify-all-frames-parameters
            '((border-width . 0)
              (internal-border-width . 0)))
+
+          ;; Set up package.el to work with MELPA
+          (require 'package)
+          (add-to-list 'package-archives
+                       '("melpa" . "https://melpa.org/packages/"))
+          (package-initialize)
+          (package-refresh-contents)
+
+          ;; Download Evil
+          (unless (package-installed-p 'evil)
+            (package-install 'evil))
+
+          ;; Enable Evil
+          (require 'evil)
+          (evil-mode 1)
         '';
       };
     };
