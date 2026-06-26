ed308882a828c64664bd1c603a36a34a05de1af0
Matthew Wilding <mbwilding@gmail.com>
2026-06-26 20:59:48 +0800

    update

--- modules/features/applications/dolphin [n]/dolphin.nix
+++ modules/features/applications/dolphin [n]/dolphin.nix
@@ -24,5 +24,29 @@
           kdePackages.kio
         ];
       };
+
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "image/png" = "imv.desktop";
+          "image/jpeg" = "imv.desktop";
+          "image/gif" = "imv.desktop";
+          "image/bmp" = "imv.desktop";
+          "image/svg+xml" = "imv.desktop";
+          "image/tiff" = "imv.desktop";
+          "image/webp" = "imv.desktop";
+          "image/x-icon" = "imv.desktop";
+          "video/3gpp" = "mpv.desktop";
+          "video/3gpp2" = "mpv.desktop";
+          "video/mp4" = "mpv.desktop";
+          "video/mpeg" = "mpv.desktop";
+          "video/ogg" = "mpv.desktop";
+          "video/quicktime" = "mpv.desktop";
+          "video/webm" = "mpv.desktop";
+          "video/x-matroska" = "mpv.desktop";
+          "video/x-msvideo" = "mpv.desktop";
+          "video/x-ms-wmv" = "mpv.desktop";
+        };
+      };
     };
 }
