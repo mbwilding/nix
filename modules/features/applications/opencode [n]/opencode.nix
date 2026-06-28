9de45174902608fc6b8202690b4572886ca78305
Matthew Wilding <mbwilding@gmail.com>
2026-06-29 05:13:37 +0800

    update

--- modules/features/applications/opencode [n]/opencode.nix
+++ modules/features/applications/opencode [n]/opencode.nix
@@ -15,6 +15,34 @@
           context = ./context.md;
           settings = {
             lsp = true;
+            model = "llama-swap/qwythos-9b";
+            small_model = "llama-swap/qwythos-9b-fast";
+            provider = {
+              llama-swap = {
+                api = "openai";
+                name = "Llama Swap";
+                options = {
+                  baseURL = "http://192.168.11.254:60000/v1";
+                  apiKey = "not-needed";
+                };
+                models = {
+                  "qwythos-9b-fast" = {
+                    name = "Qwythos 9B Fast (Q6_K)";
+                    id = "qwythos-9b-fast";
+                    tool_call = true;
+                    temperature = true;
+                    reasoning = true;
+                  };
+                  "qwythos-9b" = {
+                    name = "Qwythos 9B (Q8_0)";
+                    id = "qwythos-9b";
+                    tool_call = true;
+                    temperature = true;
+                    reasoning = true;
+                  };
+                };
+              };
+            };
             permission = {
               # allow, deny, ask
               bash = "allow";
