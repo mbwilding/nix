029a8a547fd39dbdd4f3d8e2758a05b165adb2d6
Matthew Wilding <mbwilding@gmail.com>
2026-07-05 20:43:23 +0800

    update-

--- modules/features/networking/wireguard-nona [N]/wireguard-nona.nix
+++ modules/features/networking/wireguard-nona [N]/wireguard-nona.nix
@@ -18,28 +18,28 @@
         ];
       };
 
-      networking.networkmanager.dispatcherScripts = [
-        {
-          source = pkgs.writeShellScript "wg-auto" ''
-            INTERFACE="$1"
-            ACTION="$2"
-
-            [ "$ACTION" = "up" ] || [ "$ACTION" = "down" ] || exit 0
-
-            SSID=$(${pkgs.networkmanager}/bin/nmcli -t -f active,ssid dev wifi 2>/dev/null | grep '^yes' | cut -d: -f2)
-
-             if [ "$ACTION" = "down" ]; then
-               systemctl stop wg-quick-Home
-             elif [ "$ACTION" = "up" ]; then
-               if [ "$SSID" != "${secrets.wifiHomeSsid}" ]; then
-                 systemctl start wg-quick-Home
-               else
-                 systemctl stop wg-quick-Home
-               fi
-             fi
-          '';
-          type = "basic";
-        }
-      ];
+      # networking.networkmanager.dispatcherScripts = [
+      #   {
+      #     source = pkgs.writeShellScript "wg-auto" ''
+      #       INTERFACE="$1"
+      #       ACTION="$2"
+      #
+      #       [ "$ACTION" = "up" ] || [ "$ACTION" = "down" ] || exit 0
+      #
+      #       SSID=$(${pkgs.networkmanager}/bin/nmcli -t -f active,ssid dev wifi 2>/dev/null | grep '^yes' | cut -d: -f2)
+      #
+      #        if [ "$ACTION" = "down" ]; then
+      #          systemctl stop wg-quick-Home
+      #        elif [ "$ACTION" = "up" ]; then
+      #          if [ "$SSID" != "${secrets.wifiHomeSsid}" ]; then
+      #            systemctl start wg-quick-Home
+      #          else
+      #            systemctl stop wg-quick-Home
+      #          fi
+      #        fi
+      #     '';
+      #     type = "basic";
+      #   }
+      # ];
     };
 }
