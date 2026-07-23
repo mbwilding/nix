81b0c777869ce68df4a22f1e28e122fd1c24bb4f
Matthew Wilding <mbwilding@gmail.com>
2026-07-24 01:34:26 +0800

    pwsh lsp

--- modules/features/applications/neovim [n]/lua/plugins/lspconfig.lua
+++ modules/features/applications/neovim [n]/lua/plugins/lspconfig.lua
@@ -150,7 +150,7 @@ return {
             },
             powershell_es = {
                 bundle_path = (function()
-                    return (is_nixos and "/etc/profiles/per-user/mbwilding/bin/powershell-editor-services")
+                    return (is_nixos and "/etc/profiles/per-user/mbwilding/lib/powershell-editor-services")
                         or (jit.os == "OSX" and os.getenv("HOME") .. "/.lsp/powershell_es")
                         or (jit.os == "Linux" and "/opt/powershell-editor-services")
                 end)(),
