ac3eb54411394a91ddcd5a6fb9ed74b9d75ea56f
Matthew Wilding <mbwilding@gmail.com>
2026-07-30 15:57:47 +0800

    2026-07-30-15-57-47

--- modules/nix/flake-parts/lib.nix
+++ modules/nix/flake-parts/lib.nix
@@ -31,11 +31,13 @@
           "https://attic.xuyh0120.win/lantian"
           "https://noctalia.cachix.org"
           "https://cache.nixos-cuda.org"
+          "https://yazi.cachix.org"
         ];
         extra-trusted-public-keys = [
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
           "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+          "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
         ];
         max-jobs = 1;
         # cores = 2;
