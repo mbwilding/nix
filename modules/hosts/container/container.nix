0e4505eaa896e9f27e3125c1ff812f26c41510a6
Matthew Wilding <mbwilding@gmail.com>
2026-08-03 20:15:12 +0800

    update

---
+++ modules/hosts/container/container.nix
@@ -0,0 +1,56 @@
+{ inputs, ... }:
+
+let
+  arch = "x86_64-linux";
+  hostName = "container";
+  stateVersion = "25.11";
+
+  features = [
+    # "appimage"
+    # "claudecode"
+    # "development"
+    # "podman"
+    # "proxy"
+    # "proxychains"
+    # "system-default" # Don't enable
+    # "ucodenix"
+    # "user-mbwilding"
+  ];
+
+  featureModules = inputs.self.lib.mkFeatures features;
+in
+{
+  flake.modules.nixos.${hostName} =
+    { modulesPath, ... }:
+    {
+      imports = [
+        "${modulesPath}/virtualisation/lxc-container.nix"
+        ./incus.nix
+      ];
+
+      home-manager.sharedModules = featureModules.homeManager;
+
+      networking = {
+        dhcpd.enable = false;
+        useDHCP = false;
+        useHostResolvConf = false;
+      };
+
+      systemd.network = {
+        enable = true;
+        networks."50-eth0" = {
+          matchConfig.Name = "eth0";
+          networkConfig = {
+            DHCP = "ipv4";
+            IPv6AcceptRA = true;
+          };
+          linkConfig.RequireForOnline = "routable";
+        };
+      };
+      system.stateVersion = stateVersion;
+    };
+
+  flake.nixosConfigurations = inputs.self.lib.mkNixOS arch hostName;
+
+  flake.homeConfigurations = inputs.self.lib.mkHomeManager arch hostName featureModules.homeManager;
+}
