851e0b0c7309aa45d3514185a1e080b48d8f714f
Matthew Wilding <mbwilding@gmail.com>
2026-08-05 20:07:18 +0800

    2026-08-05-20-07-18

--- modules/features/configs/package-managers [n]/package-managers.nix
+++ modules/features/configs/package-managers [n]/package-managers.nix
@@ -12,7 +12,7 @@
             <packageSources>
               <add key="nuget.org" value="https://api.nuget.org/v3/index.json" protocolVersion="3" />
               <!-- <add key="github" value="https://nuget.pkg.github.com/${secrets.workName}/index.json" /> -->
-              <add key="artifactory" value="https://artifactory.internal.${secrets.workName}.delivery/artifactory/api/nuget/v3/nuget-github/index.json" />
+              <add key="artifactory" value="https://artifactory.internal.${secrets.workName}.delivery/artifactory/api/nuget/v3/nuget-all/index.json" />
             </packageSources>
             <packageSourceCredentials>
               <github>
