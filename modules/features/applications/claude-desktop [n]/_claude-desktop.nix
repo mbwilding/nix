2fafbbfc84a0ef537c0337d67d56a631ee4f279a
Matthew Wilding <mbwilding@gmail.com>
2026-09-24 09:36:07 +0800

    update

--- modules/features/applications/claude-desktop [n]/_claude-desktop.nix
+++ modules/features/applications/claude-desktop [n]/_claude-desktop.nix
@@ -42,16 +42,16 @@
 
 let
   pname = "claude-desktop";
-  version = "2.2553.1";
+  version = "2.2553.13";
 
   sources = {
     "x86_64-linux" = {
       url = "https://downloads.claude.ai/claude-desktop/apt/stable/pool/main/c/claude-desktop/claude-desktop_${version}_amd64.deb";
-      hash = "sha256-ZwD92E53prjJORLC9p610eQPqZvNnTf0OPgJ7ypv5vg=";
+      hash = "sha256-AqlanRM0csG3c8jp297lZoG4FkUAF4CDvYtTudEdEJ8=";
     };
     "aarch64-linux" = {
       url = "https://downloads.claude.ai/claude-desktop/apt/stable/pool/main/c/claude-desktop/claude-desktop_${version}_arm64.deb";
-      hash = "sha256-AAOm+WBaIQ8Dw4Zw1izVnHEVPCcCqkQn5Mq+Li5fM5A=";
+      hash = "sha256-/b2cmAn0sxR4qyHgUf/CvpiVXqcvF6VKhCARXGY8s6c=";
     };
   };
 
