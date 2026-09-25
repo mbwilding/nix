9fbb6ac518f1868ef59dd6f8a5ea5b225576ff57
Matthew Wilding <mbwilding@gmail.com>
2026-09-26 01:07:59 +0800

    2026-09-26-01-07-59

--- modules/features/applications/neovim [n]/lua/plugins/oil.lua
+++ modules/features/applications/neovim [n]/lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 ---@type zpack.Spec
 return {
     -- "stevearc/oil.nvim",
-    "barrettruth/canola.nvim", -- "https://forge.barrettruth.com/barrettruth/canola.nvim",
+    "https://forge.barrettruth.com/barrettruth/canola.nvim",
     lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons",
