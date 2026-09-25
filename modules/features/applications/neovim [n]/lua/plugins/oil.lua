d7c21708452e474cc675696e6506f58803205eaf
Matthew Wilding <mbwilding@gmail.com>
2026-09-25 16:52:59 +0800

    update

--- modules/features/applications/neovim [n]/lua/plugins/oil.lua
+++ modules/features/applications/neovim [n]/lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 ---@type zpack.Spec
 return {
     -- "stevearc/oil.nvim",
-    "barrettruth/canola.nvim", -- Drop in replacement with additional functionality
+    "https://forge.barrettruth.com/barrettruth/canola.nvim",
     lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons",
